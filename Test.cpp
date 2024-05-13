db9a78698096c0a582ee279bcabf6b32ac092a46
KitChaos <cmeow176@gmail.com>
2024-05-13 14:04:20 -0400

    Added some more stuff

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Window/WindowStyle.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
@@ -26,6 +27,14 @@ struct Vector2{
       return newVec;
 
    }
+   Vector2 operator/(float Amt){
+      Vector2 newVec(0,0);
+      newVec.x = x/Amt;
+      newVec.y = y/Amt;
+      return newVec;
+
+   }
+   
 };
 class Ball{
     public:
@@ -33,6 +42,7 @@ class Ball{
   Vector2 Velocity = Vector2(0,0);  
   sf::Color MyColor=sf::Color(255,255,255);
   float Size = 4;
+  bool HasGravity;
   Ball(float X, float Y){
     Position = Vector2(X,Y);
   }
@@ -41,12 +51,13 @@ int main()
 {
     std::vector<Ball> Balls;
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Fullscreen);
-    for (int i=0; i<1000; i++) {
+    for (int i=0; i<150; i++) {
            Ball newBall = Ball(500,500);
            //Ball((float)rand()/RAND_MAX*window.getSize().x,(float)rand()/RAND_MAX*window.getSize().y);
-            newBall.Velocity = Vector2((float)rand()/RAND_MAX-(float)rand()/RAND_MAX,(float)rand()/RAND_MAX-(float)rand()/RAND_MAX-(float)rand()/RAND_MAX);
+            newBall.Velocity = Vector2(((float)rand()/RAND_MAX-(float)rand()/RAND_MAX),((float)rand()/RAND_MAX-(float)rand()/RAND_MAX));
             newBall.MyColor =sf::Color((float)rand()/RAND_MAX*255,(float)rand()/RAND_MAX*255,(float)rand()/RAND_MAX*255);
-            newBall.Size = (float)rand()/RAND_MAX*14;
+            newBall.Size = (float)rand()/RAND_MAX*12+2;
+            newBall.HasGravity = false;
             Balls.emplace_back(newBall);
             
     }
@@ -73,16 +84,36 @@ int main()
             
             shape.setFillColor(ball.MyColor);
             shape.setPosition(ball.Position.x, ball.Position.y);
-            ball.Position = ball.Position+ball.Velocity;
-            if(ball.Position.x <0||ball.Position.x>window.getSize().x)
+            
+            
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                ball.Velocity = ball.Velocity+(Vector2((sf::Mouse::getPosition(window).x-ball.Position.x),((sf::Mouse::getPosition(window).y-ball.Position.y))))*-1;
+            }
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+            {
+                ball.Velocity = ball.Velocity+ (Vector2((sf::Mouse::getPosition(window).x-ball.Position.x),((sf::Mouse::getPosition(window).y-ball.Position.y))));
+            }
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Middle)){
+                ball.Velocity = Vector2(0,0);
+            }
+            if(ball.Position.x+ball.Velocity.x/100 <0||ball.Position.x+ball.Velocity.x/100>window.getSize().x)
             {
                 ball.Velocity.x *= -1;
             }
-            if(ball.Position.y<0||ball.Position.y>window.getSize().y){
+            if(ball.Position.y+ball.Velocity.y/100<0||ball.Position.y+ball.Velocity.y/100>window.getSize().y){
                 //invert x if wall, y if ground
-            ball.Velocity.y *= -1;
+                ball.Velocity.y *= -1;
             
             }
+            
+            if(ball.HasGravity){
+                    ball.Velocity.y += 5;
+            }
+            
+            
+
+            ball.Position = ball.Position+ball.Velocity/100;
             window.draw(shape);
             
         }
